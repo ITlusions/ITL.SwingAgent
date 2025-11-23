eab0f811835df3aa9d2b20ae3af6eca0deeee152
Niels Weistra <n.weistra@itlusions.nl>
2025-11-23 14:04:51 -0800

    Improve Ross Cameron agent data handling

--- src/swing_agent/__init__.py
+++ src/swing_agent/__init__.py
@@ -1,4 +1,25 @@
 from .agent import SwingAgent
 from .risk import sized_quantity, total_risk, can_open
+from .ross_cameron_agent import (
+    NotificationChannel,
+    NotificationConfig,
+    NotificationDispatcher,
+    PillarAssessment,
+    PillarName,
+    RossCameronAgent,
+    RossCameronScreeningResult,
+)
 
-__all__ = ["SwingAgent", "sized_quantity", "total_risk", "can_open"]
+__all__ = [
+    "SwingAgent",
+    "sized_quantity",
+    "total_risk",
+    "can_open",
+    "RossCameronAgent",
+    "NotificationDispatcher",
+    "NotificationConfig",
+    "NotificationChannel",
+    "PillarName",
+    "PillarAssessment",
+    "RossCameronScreeningResult",
+]
